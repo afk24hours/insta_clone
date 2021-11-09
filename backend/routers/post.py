306fa91c3b56011348b5e40aed9c48c31ce63b23
Serikbol <serikbol.ktl@gmail.com>
2021-11-09 23:23:37 +0500

    second commit

--- backend/routers/post.py
+++ backend/routers/post.py
@@ -1,7 +1,7 @@
 import random
 import string
 import shutil
-
+from typing import List
 from fastapi import APIRouter, Depends, status, UploadFile, File
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm.session import Session
@@ -9,7 +9,6 @@ from auth.oauth2 import get_current_user
 from routers.schemas import PostBase, PostDisplay, UserAuth
 from db.database import get_db
 from db import db_post
-from typing import List
 
 
 router = APIRouter(
@@ -23,7 +22,7 @@ image_url_types = ['absolute', 'relative']
 def create(request: PostBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
     if not request.image_url_type in image_url_types:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, 
-        detail="Parameter of image_url_type can be only 'absolute' or 'relative'.")
+                            detail="Parameter of image_url_type can be only 'absolute' or 'relative'.")
     return db_post.create(db, request)
 
 @router.get('/all', response_model=List[PostDisplay])
@@ -33,11 +32,10 @@ def get_posts(db: Session = Depends(get_db)):
 @router.post('/image')
 def upload_image(image: UploadFile = File(...), current_user: UserAuth = Depends(get_current_user)):
     letters = string.ascii_letters
-    rand_string = ''.join(random.choice(letters) for i in range(8))
+    rand_string = ''.join(random.choice(letters) for i in range(8)) # ADDS RANDOM 8 LETTERS TO NAME
     new = f'_{rand_string}.'
     filename = new.join(image.filename.rsplit('.', 1))
     path = f'images/{filename}'
-
     with open(path, "w+b") as buffer:
         shutil.copyfileobj(image.file, buffer)
     return {'filename': path}
---
+++ frontend/src/App.js
@@ -0,0 +1,237 @@
+import { Button, makeStyles, Modal, Input } from '@material-ui/core';
+import React, {useState, useEffect} from 'react'
+import './App.css';
+import Post from './Post';
+
+const BASE_URL = 'http://localhost:8000/'
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    position: 'absolute',
+    width: 400,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
+  }
+}))
+
+function App() {
+  const classes = useStyles();
+
+
+  const [posts, setPosts] = useState([]);
+  const [OpenSignIn, setOpenSignIn] = useState(false);
+  const [OpenSignUp, setOpenSignUp] = useState(false);
+  const [modalStyle, setModalStyle] = useState(getModalStyle)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [authToken, setAuthToken] = useState(null)
+  const [authTokenType, setAuthTokenType] = useState(null);
+  const [userId, setUserId] = useState('');
+  const [email, setEmail] = useState('')
+
+  useEffect(() => {
+    setAuthToken(window.localStorage.getItem('authToken'));
+    setAuthTokenType(window.localStorage.getItem('authTokenType'))
+    setUsername(window.localStorage.getItem('username'))
+    setUserId(window.localStorage.getItem('userId'))
+  }, [])
+
+  useEffect(() => {
+    authToken
+      ? window.localStorage.setItem('authToken', authToken)
+      : window.localStorage.removeItem('authToken')
+    authTokenType
+      ? window.localStorage.setItem('authTokenType', authTokenType)
+      : window.localStorage.removeItem('authTokenType')
+    username
+      ? window.localStorage.setItem('username', username)
+      : window.localStorage.removeItem('username')
+    userId
+      ? window.localStorage.setItem('userId', userId)
+      : window.localStorage.removeItem('userId')
+
+  }, [authToken, authTokenType, userId])
+
+  useEffect(() => {
+    fetch(BASE_URL + 'post/all')
+      .then(response => {
+        const json = response.json()
+        console.log(json)
+        if (response.ok) {
+          return json
+        }
+        throw response
+      })
+      .then(data => {
+        const result = data.sort((a,b) => {
+          const t_a = a.timestamp.split(/[-T:]/);
+          const t_b = b.timestamp.split(/[-T:]/);
+          const d_a = new Date(Date.UTC(t_a[0], t_a[1]-1, t_a[2], t_a[3], t_a[4], t_a[5]))
+          const d_b = new Date(Date.UTC(t_b[0], t_b[1]-1, t_b[2], t_b[3], t_b[4], t_b[5]))
+          return d_b - d_a
+        })
+        return result
+      })
+      .then(data => {
+        setPosts(data)
+      })
+      .catch(error => {
+        console.log();
+        alert(error)
+      })
+  }, [])
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    let formData = new FormData();
+    formData.append('username', username)
+    formData.append('password', password)
+
+    const requestOptions = {
+      method: 'POST',
+      body: formData
+    }
+
+    fetch(BASE_URL + 'login', requestOptions)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response
+      })
+      .then(data => {
+        console.log(data);
+        setAuthToken(data.access_token)
+        setAuthTokenType(data.token_type)
+        setUserId(data.user_id)
+        setUsername(data.username)
+      })
+      .catch(error => {
+        console.log(error);
+        alert(error)
+      })
+
+    setOpenSignIn(false);
+  }
+
+  const signOut = (event) => {
+    setAuthToken(null)
+    setAuthTokenType(null)
+    setUserId('')
+    setUsername('')
+  }
+
+  const signUp = (event) => {
+
+  }
+
+  return (
+    <div className="app">
+      <Modal
+        open={OpenSignIn}
+        onClose={() => setOpenSignIn(false)}>
+
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app_signin">
+            <center>
+              <img className="app_headerImage"
+                src="https://cdn.icon-icons.com/icons2/2699/PNG/512/instagram_logo_icon_170643.png"
+                alt="Instagram"/>
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+            <Button
+              type="submit"
+              onClick={signIn}>Login</Button>
+          </form>
+        </div>
+
+      </Modal>
+
+      <Modal
+        open={OpenSignUp}
+        onClose={() => setOpenSignUp(false)}>
+
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app_signin">
+            <center>
+              <img className="app_headerImage"
+                src="https://cdn.icon-icons.com/icons2/2699/PNG/512/instagram_logo_icon_170643.png"
+                alt="Instagram"/>
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+            <Button
+              type="submit"
+              onClick={signUp}>Signup</Button>
+          </form>
+        </div>
+
+      </Modal>
+
+      <div className="app_header">
+        <img className="app_headerImage"
+          src="https://cdn.icon-icons.com/icons2/2699/PNG/512/instagram_logo_icon_170643.png"
+          alt="Instagram"/>
+
+        {authToken ? (
+          <Button onClick={() => signOut()}>Logout</Button>
+        ) : (
+          <div>
+            <Button onClick={() => setOpenSignIn(true)}>Login</Button>
+            <Button onClick={() => setOpenSignUp(true)}>Signup</Button>
+          </div>
+        )
+        }
+      </div>
+      <div className="app_posts">
+        {
+          posts.map(post => (
+            <Post
+            post = {post}
+            />
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+
+
+export default App;
